5bceedf0b20153f8ec18bd9f577d603d690b80ff
Alexandre PASCAL <alpascal@e3r13p2.clusters.42paris.fr>
2022-05-22 17:35:01 +0200

    22/05

--- srcs/vector/includes/vector.hpp
+++ srcs/vector/includes/vector.hpp
@@ -11,33 +11,81 @@ namespace ft
     template< class T, class Alloc = std::allocator<T> >
     class vector
     {
-        typedef Alloc                           allocator_type;
-        typedef T                               value_type;
-        typedef allocator_type::reference       reference;
-        typedef allocator_type::const_reference const_reference;
-        typedef allocator_type::pointer         pointer;
-        typedef allocator_type::const_pointer   const_pointer;
-
-        private:
-            size_t                                             _size_type;
-            ptrdiff_t                                          _difference_type;
-            typedef standard_iterator< pointer, vector >       _iterator;
-            typedef standard_iterator< const_pointer, vector > _const_iterator;
-            typedef reverse_iterator< const_iterator >         _const_reverse_iterator;
-            typedef reverse_iterator< iterator >               _reverse_iterator;
-            
+        
+        private:        
         protected:
+            Alloc           _alloc;
+            size_t          _size;
+            size_t          _capa;
+            T*              _cont;        
         public:
+            typedef Alloc                                       allocator_type;
+            typedef T                                           value_type;
+            typedef value_type&                                 reference;
+            typedef const value_type&                           const_reference;
+            typedef value_type*                                 pointer;
+            typedef const value_type*                           const_pointer;
+            typedef std::size_t                                 size_type;
+            typedef std::ptrdiff_t                              difference_type;
+            typedef standard_iterator< pointer, vector >        iterator;
+            typedef standard_iterator< const_pointer, vector >  const_iterator;
+            typedef reverse_iterator< const_iterator >          const_reverse_iterator;
+            typedef reverse_iterator< iterator >                reverse_iterator;
+
 //  Constructors and destructors
-            explicit vector (const allocator_type& alloc = allocator_type()){};
+            explicit vector (const allocator_type& alloc = allocator_type())
+            {
+                _alloc = alloc;
+                _size = 0;
+                _capa = 0;
+                _cont = NULL;
+            }
+
             explicit vector (size_type n, const value_type& val = value_type(),
-                    const allocator_type& alloc = allocator_type()){};	
+                    const allocator_type& alloc = allocator_type())
+            {
+                pointer tmp;
+                _alloc = alloc;
+                _size = n;
+                _capa = n;
+                _cont = alloc.allocate(n, 0);
+                for (int i = 0; i < n; i++)
+                    alloc.construct(&_cont[i], val[i]);
+            }
+
             template <class InputIterator>
             vector (InputIterator first, InputIterator last,
-                    const allocator_type& alloc = allocator_type()){};	
-            vector (const vector& x){};
-            ~vector(){};
-//  Iterrators
+                    const allocator_type& alloc = allocator_type())
+            {
+                pointer tmp;
+                _alloc = alloc;
+                _size = last - first;
+                _capa = last - first;
+                _cont = alloc.allocate(_size, 0);
+            
+            }
+
+            vector (const vector& x)
+            {
+                _alloc = x.get_allocator();
+                _size = x._size;
+                _capa = x._capa;
+                _cont = x._cont;
+            }
+
+            ~vector()
+            {
+                allocator.deallocate(_cont, size);
+            }
+
+            vector& operator= (const vector& x)
+            {
+                _alloc = x.get_allocator();
+                _size = x.size;
+                _capa = x._capa;
+                _cont = x._cont;
+            }
+//  Iterators
             iterator begin();
             const_iterator begin() const;
             iterator end();
@@ -47,23 +95,29 @@ namespace ft
             reverse_iterator rend();
             const_reverse_iterator rend() const;
 //  Capacity
-            size_type size() const;
+            size_type size() const{return (*this->_size);}
             size_type max_size() const;
             void resize (size_type n, value_type val = value_type());
-            size_type capacity() const;
-            bool empty() const;
+            size_type capacity() const{return (*this->_capa);}
+            bool empty() const
+            {
+                if (_cont)
+                    return (0);
+                return (1);
+            }
             void reserve (size_type n);
 //  Element access
-            reference operator[] (size_type n);
-            const_reference operator[] (size_type n) const;
-            reference at (size_type n);
-            const_reference at (size_type n) const;
-            reference front();
-            const_reference front() const;
-            reference back();
-            const_reference back() const;
-            template <class InputIterator>
+            reference operator[] (size_type n){return (_cont[n]);}
+            const_reference operator[] (size_type n) const{return (_cont[n]);}
+            reference at (size_type n){return (&_cont[n]);}
+            const_reference at (size_type n) const{return (&_cont[n]);}
+            reference front(){return (&_cont[_size]);}
+            const_reference front() const{return (&_cont[_size]);}
+            reference back(){return (&_cont[0]);}
+            const_reference back() const{return (&_cont[0]);}
+
 // Modifiers
+            template <class InputIterator>
             void assign (InputIterator first, InputIterator last);	
             void assign (size_type n, const value_type& val);
             void push_back (const value_type& val);
@@ -97,7 +151,11 @@ namespace ft
     template <class T, class Alloc>
     bool operator== (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs)
     {
-        return ();
+        if (lhs._size == rhs._size)
+            for (int i = 0; (i < lhs._size && i < rhs._size); i++)
+                if (rhs._cont[i] != lhs._cont[i])
+                    return (0);
+        return (1);
     }
 
     template <class T, class Alloc>
@@ -109,7 +167,14 @@ namespace ft
     template <class T, class Alloc>
     bool operator<  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs)
     {
-        return ();
+        for (int i = 0; i < lhs._size; i++)
+        {
+            if (i == rhs._size || rhs._cont < lhs._cont)
+                return (0)
+            else if (lhs._cont < rhs._cont)
+                return (1);
+        }
+        return (0);
     }
 
     template <class T, class Alloc>
