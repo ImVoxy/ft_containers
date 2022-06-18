70a7c46eb1c95f47b5a6e5280a008ad5cdd32455
Alexandre PASCAL <alpascal@e3r13p6.clusters.42paris.fr>
2022-06-18 17:28:44 +0200

    stack vector and others are ok and leaks free

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -12,22 +12,22 @@
 int main()
 {
     std::cout << std::boolalpha;
-    // std::cout << std::endl << "\033[1;32m     STACK TESTS     \033[0m" << std::endl << std::endl;
-    // stack_test();
-    // std::cout << std::endl << "\033[1;32m     VECTOR TESTS     \033[0m" << std::endl << std::endl;
-    // vector_test();
-    // std::cout << std::endl << "\033[1;32m     ITERATOR TESTS     \033[0m" << std::endl << std::endl;
-    // iterator_test();
+    std::cout << std::endl << "\033[1;32m     STACK TESTS     \033[0m" << std::endl << std::endl;
+    stack_test();
+    std::cout << std::endl << "\033[1;32m     VECTOR TESTS     \033[0m" << std::endl << std::endl;
+    vector_test();
+    std::cout << std::endl << "\033[1;32m     ITERATOR TESTS     \033[0m" << std::endl << std::endl;
+    iterator_test();
     std::cout << std::endl << "\033[1;32m     REVERSE_ITERATOR TESTS     \033[0m" << std::endl << std::endl;
     reverse_iterator_test();
-    // std::cout << std::endl << "\033[1;32m     PAIR TESTS     \033[0m" << std::endl << std::endl;
-    // pair_test();
-    // std::cout << std::endl << "\033[1;32m     LEXICOGRAPHICAL_COMPARE TESTS     \033[0m" << std::endl << std::endl;
-    // lexicographical_compare_test();
-    // std::cout << std::endl << "\033[1;32m     ENABLE_IF TESTS     \033[0m" << std::endl << std::endl;
-    // enable_if_test();
-    // std::cout << std::endl << "\033[1;32m     IS INTEGRAL TESTS     \033[0m" << std::endl << std::endl;
-    // is_integral_test();
+    std::cout << std::endl << "\033[1;32m     PAIR TESTS     \033[0m" << std::endl << std::endl;
+    pair_test();
+    std::cout << std::endl << "\033[1;32m     LEXICOGRAPHICAL_COMPARE TESTS     \033[0m" << std::endl << std::endl;
+    lexicographical_compare_test();
+    std::cout << std::endl << "\033[1;32m     ENABLE_IF TESTS     \033[0m" << std::endl << std::endl;
+    enable_if_test();
+    std::cout << std::endl << "\033[1;32m     IS INTEGRAL TESTS     \033[0m" << std::endl << std::endl;
+    is_integral_test();
 
     return (0);
 }
